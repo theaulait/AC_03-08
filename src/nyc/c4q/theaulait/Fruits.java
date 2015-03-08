12ae9428196b60ca743b741d1833cde0cf50b6f3
theaulait <vanice.yee@gmail.com>
2015-03-08 11:12:41 -0400

    updates

---
+++ src/nyc/c4q/theaulait/Fruits.java
@@ -0,0 +1,42 @@
+package nyc.c4q.theaulait;
+
+/**
+ * Created by c4q-vanice on 3/8/15.
+ */
+import java.util.Scanner;
+
+public class Fruits {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String color, flavor;
+        System.out.println("What's your favorite fruit?");
+        String fruits =  scanner.next();
+
+        if (fruits.equals("banana")){
+            color = "yellow";
+            flavor = "sweet";
+        }
+        else if (fruits.equals("grapefruit")){
+            color = "pink";
+            flavor = "bitter";
+        }
+        else if (fruits.equals("lemon")){
+            color = "yellow";
+            flavor = "sour";
+        }
+        else if (fruits.equals("apple")){
+            color = "red";
+            flavor = "sweet";
+        }
+        else if (fruits.equals("strawberry")){
+            color = "red";
+            flavor = "sweet";
+        }
+        else {
+            System.out.println("I don't know that fruit.");
+            return;
+        }
+        System.out.println("A " + fruits + " is " + color + " and tastes " + flavor + ".");
+    }
+
+}
--- src/nyc/c4q/theaulait/Main.java
+++ src/nyc/c4q/theaulait/Main.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("what's your score?");
-        int score =scanner.nextInt();
-        if (score > 100)
+        int score = scanner.nextInt();
+        if (score > 100) {
             System.out.println("Wow!");
-        if (score == 100)
+        }
+        if (score == 100) {
             System.out.println("Perfect score!");
-        else if (score >= 90);
+        } else if (score >= 90) {
             System.out.println("Great!");
-        else if (score >= 75);
+        } else if (score >= 75) {
             System.out.println("You passed.");
-        else
+        } else {
             System.out.println("You failed. :(");
+        }
+            System.out.println("We're done!");
 
-        System.out.println("We're done!");
 
     }
 }
